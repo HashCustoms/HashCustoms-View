4077719568fef83a49b2da64554c876c2fcd2237
Shubham Omar <shubhamomar53@gmail.com>
2016-11-27 13:14:31 +0530

    Home page radio button added for prototype number selection, Available number will only appear in design.

--- js/fx-script.js
+++ js/fx-script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-    var $this, $imgOrder, $galleryImgObject = $(".gallery-img"),
+    var newData, $this, $imgOrder, $radioObject = $("[type='radio']"),
+        $bookBtn = $(".book-now"),
+        $galleryImgObject = $(".gallery-img"),
         $carousel = $("#gallery-carousel"),
         $imgViewOverlay = $(".full-view-overlay"),
         $closePreview = $(".full-view-close"),
         $parllaxObject = $(".parallax-window");
-    $parllaxObject.parallax({ speed: 0.3 })
+    $radioObject.prop('disabled', true).parent().addClass("hide");
+    $parllaxObject.parallax({ speed: 0.3 });
     $carousel.carousel('pause');
     $galleryImgObject.on('click', function() {
         $this = $(this);
@@ -17,5 +20,20 @@ $(document).ready(function() {
     $closePreview.on('click', function() {
         $imgViewOverlay.removeClass("view");
         setTimeout(function() { $imgViewOverlay.addClass("hide"); }, 200);
+    });
+    $bookBtn.on('click', function() {
+        $.ajax({
+            type: "GET",
+            url: "dataList.json",
+            dataType: 'json',
+            success: function(data) {
+                var id;
+                newData = data.available;
+                $(newData).each(function(i, value) {
+                    id = "#num" + value;
+                    $(id).prop('disabled', false).parent().removeClass("hide");
+                });
+            }
+        });
     });
 });
